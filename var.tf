d0476ac5cf4cd9c4e02cf2aef8c2d6cc9ed17878
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2025-01-21 16:36:35 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  default = "ami-0583d8c7a9c35822c"
+  description = "This is ami description"
+}
+variable "instance_type" {
+  default = "t2.micro"
+  description = "This is instance type"
+}
+
+variable "tag_name" {
+}
