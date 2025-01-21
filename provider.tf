f9672ef71edbcc0329bc808b22107d841c68a1a6
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2025-01-21 18:48:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.68.0"
-    }
-  }
-}
-
 provider "aws" {
-  # Configuration options
-  region = "us-east-1"
+  region = "us-east-1" # Update this with your region
 }
